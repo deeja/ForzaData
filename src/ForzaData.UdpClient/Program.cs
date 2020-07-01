5fa061eece95a95237d65bb5e08f1d338344d633
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2020-07-01 12:39:56 +0100

    Clean up the UI a little, but not much

--- src/ForzaData.UdpClient/Program.cs
+++ src/ForzaData.UdpClient/Program.cs
@@ -14,18 +14,15 @@ namespace ForzaData.UdpClient
         {
             byte[] toDisplay = null;
 
-            Task consoleUpdater = Task.Run(async () =>
+            var consoleUpdater = Task.Run(async () =>
             {
                 while (true)
                 {
                     if (toDisplay != null)
                     {
-                        //Console.Clear();
-                        Console.SetCursorPosition(0, 2);
-
+                        Console.SetCursorPosition(0, 0);
                         Console.Write(StatusScreen(toDisplay));
                         toDisplay = null;
-
                     }
 
                     await Task.Delay(300);
@@ -52,7 +49,7 @@ namespace ForzaData.UdpClient
                 Console.WriteLine("Listening... ");
                 while (true)
                 {
-                    await client.ReceiveAsync().ContinueWith((receive) =>
+                    await client.ReceiveAsync().ContinueWith(receive =>
                     {
                         var resultBuffer = receive.Result.Buffer;
                         if (resultBuffer.Length != 311 || toDisplay != null)
@@ -61,29 +58,33 @@ namespace ForzaData.UdpClient
                         }
 
                         var stuff = new byte[resultBuffer.Length];
-                        resultBuffer.CopyTo(stuff,0);
+                        resultBuffer.CopyTo(stuff, 0);
                         toDisplay = stuff;
                     });
                 }
             });
 
 
-            Task.WaitAll(senderTask, receiverTask);
+            Task.WaitAll(senderTask, receiverTask, consoleUpdater);
         }
 
 
         private static string StatusScreen(byte[] packet)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Timestamp: {packet.TimestampMs()}".PadRight(50));
-            sb.AppendLine($"Dist: {packet.Distance()}                                             ");
+            var sb = new StringBuilder();
+            sb.AppendLine($"In Race: {packet.IsRaceOn()}      Timestamp: {packet.TimestampMs()} ");
+            sb.AppendLine($"Distance: {packet.Distance()}m                                      ");
             sb.AppendLine($"Current Lap: {packet.CurrentLapTime()}, Race: {packet.CurrentRaceTime()}");
-            sb.AppendLine($"Pos: {packet.PositionX()}, {packet.PositionY()}, {packet.PositionZ()}     ");
-            sb.AppendLine($"Gear: {packet.Gear()}                                                 ");
-            sb.AppendLine($"Brake: {packet.Brake()} Accel: {packet.Accelerator()}                   ");
-            sb.AppendLine($"Speed: {packet.Speed()}    Steering:  {packet.Steer()} {BitConverter.ToString(packet, 308,1)}                 ");
-            sb.AppendLine($"Race position: {packet.RacePosition()}                                ");
-            sb.AppendLine($"Racing: {packet.IsRaceOn()}                                           ");
+            sb.AppendLine($"Coords: {packet.PositionX()}, {packet.PositionY()}, {packet.PositionZ()}     ");
+            sb.AppendLine($"Gear: {packet.Gear()}  Speed: {packet.Speed()}                                               ");
+            sb.AppendLine($"Brake: {packet.Brake()} Accel: {packet.Accelerator()} Steering:  {packet.Steer()}                   ");
+            sb.AppendLine($"Race position: {packet.RacePosition()}                               ");
+            sb.AppendLine("                                                                      ");
+            sb.AppendLine("                                                                      ");
+            sb.AppendLine("                                                                      ");
+            sb.AppendLine("                                                                      ");
+            sb.AppendLine("                                                                      ");
+            sb.AppendLine("                                                                      ");
             return sb.ToString();
         }
     }
